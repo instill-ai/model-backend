2d14c7608fa4900e9a33571a2e9d003fda116220
Phelan164 <phelan.nguyen@instill.tech>
2022-02-22 15:58:15 +0700

    feat: support prediction for url/base64 image

--- internal/constants.go
+++ internal/constants.go
@@ -4,3 +4,15 @@ var CVTasks = map[string]int{
 	"cls": 1,
 	"det": 2,
 }
+
+const MaxBatchSize int = 32
+
+const (
+	_  = iota
+	KB = 1 << (10 * iota)
+	MB
+	GB
+	TB
+)
+
+const MaxImageSizeBytes int = 4 * MB
