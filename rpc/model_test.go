99bd345ce80d1403396e100c32ade84c598ce5c7
Phelan164 <phelan.nguyen@instill.tech>
2022-03-22 02:18:12 +0700

    refactor: clean model_test.go in rpc

--- rpc/model_test.go
+++ rpc/model_test.go
@@ -2,65 +2,15 @@ package rpc
 
 import (
 	"context"
-	"log"
-	"net"
 	"testing"
 	"time"
 
-	"github.com/gogo/status"
 	gomock "github.com/golang/mock/gomock"
-	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
-	grpc_recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
 	model "github.com/instill-ai/protogen-go/model/v1alpha"
 	"github.com/stretchr/testify/assert"
-	grpc "google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	metadata "google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/test/bufconn"
 )
 
 const NAMESPACE = "local-user"
-
-// RecoveryInterceptor - panic handler
-func recoveryInterceptorOpt() grpc_recovery.Option {
-	return grpc_recovery.WithRecoveryHandler(func(p interface{}) (err error) {
-		return status.Errorf(codes.Unknown, "panic triggered: %v", p)
-	})
-}
-
-// CustomInterceptor - append metadatas for unary
-func unaryAppendMetadataInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-	md, ok := metadata.FromIncomingContext(ctx)
-	if !ok {
-		return nil, status.Error(codes.Internal, "can not extract metadata")
-	}
-
-	md.Append("Username", "local-user")
-
-	newCtx := metadata.NewIncomingContext(ctx, md)
-
-	h, err := handler(newCtx, req)
-
-	return h, err
-}
-
-// CustomInterceptor - append metadatas for stream
-func streamAppendMetadataInterceptor(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
-	md, ok := metadata.FromIncomingContext(stream.Context())
-	if !ok {
-		return status.Error(codes.Internal, "can not extract metadata")
-	}
-
-	md.Append("Username", "local-user")
-
-	newCtx := metadata.NewIncomingContext(stream.Context(), md)
-	wrapped := grpc_middleware.WrapServerStream(stream)
-	wrapped.WrappedContext = newCtx
-
-	err := handler(srv, wrapped)
-
-	return err
-}
 
 func TestModelService_Readiness(t *testing.T) {
 	t.Run("Readiness", func(t *testing.T) {
@@ -107,39 +57,4 @@ func TestModelService_Liveness(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, liveRes.Status, model.LivenessResponse_SERVING_STATUS_SERVING)
 	})
-}
-
-func dialer(t *testing.T) func(context.Context, string) (net.Conn, error) {
-	listener := bufconn.Listen(1024 * 1024)
-
-	ctrl := gomock.NewController(t)
-
-	grpcServerOpts := []grpc.ServerOption{
-		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(
-			streamAppendMetadataInterceptor,
-			grpc_recovery.StreamServerInterceptor(recoveryInterceptorOpt()),
-		)),
-		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
-			unaryAppendMetadataInterceptor,
-			grpc_recovery.UnaryServerInterceptor(recoveryInterceptorOpt()),
-		)),
-	}
-
-	server := grpc.NewServer(grpcServerOpts...)
-
-	mockModelService := NewMockModelService(ctrl)
-	mockTritonService := NewMockTritonService(ctrl)
-	modelServiceHandler := NewServiceHandlers(mockModelService, mockTritonService)
-
-	model.RegisterModelServiceServer(server, modelServiceHandler)
-
-	go func() {
-		if err := server.Serve(listener); err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	return func(context.Context, string) (net.Conn, error) {
-		return listener.Dial()
-	}
 }
