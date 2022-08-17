066a13c4fff98989bf6184c346447f1340e98cb3
Phelan164 <phelan.nguyen@instill.tech>
2022-08-17 23:59:04 +0700

    chore: uncomment reporter code segment

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -10,6 +10,7 @@ import (
 	"regexp"
 	"strings"
 	"syscall"
+	"time"
 
 	"github.com/go-redis/redis/v9"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
@@ -32,10 +33,14 @@ import (
 	"github.com/instill-ai/model-backend/pkg/handler"
 	"github.com/instill-ai/model-backend/pkg/repository"
 	"github.com/instill-ai/model-backend/pkg/service"
+	"github.com/instill-ai/model-backend/pkg/usage"
+	"github.com/instill-ai/x/repo"
 
 	database "github.com/instill-ai/model-backend/internal/db"
+	mgmtPB "github.com/instill-ai/protogen-go/vdp/mgmt/v1alpha"
 	modelPB "github.com/instill-ai/protogen-go/vdp/model/v1alpha"
-	// usageclient "github.com/instill-ai/usage-client/client"
+	usagePB "github.com/instill-ai/protogen-go/vdp/usage/v1alpha"
+	usageclient "github.com/instill-ai/usage-client/client"
 )
 
 func grpcHandlerFunc(grpcServer *grpc.Server, gwHandler http.Handler, CORSOrigins []string) http.Handler {
@@ -57,24 +62,24 @@ func grpcHandlerFunc(grpcServer *grpc.Server, gwHandler http.Handler, CORSOrigin
 	)
 }
 
-// func startReporter(ctx context.Context, usageServiceClient usagePB.UsageServiceClient, r repository.Repository, mu mgmtPB.UserServiceClient, rc *redis.Client) {
-// 	logger, _ := logger.GetZapLogger()
+func startReporter(ctx context.Context, usageServiceClient usagePB.UsageServiceClient, r repository.Repository, mu mgmtPB.UserServiceClient, rc *redis.Client) {
+	logger, _ := logger.GetZapLogger()
 
-// 	version, err := repo.ReadReleaseManifest("release-please/manifest.json")
-// 	if err != nil {
-// 		logger.Fatal(err.Error())
-// 	}
+	version, err := repo.ReadReleaseManifest("release-please/manifest.json")
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
 
-// 	go func() {
-// 		time.Sleep(5 * time.Second)
+	go func() {
+		time.Sleep(5 * time.Second)
 
-// 		usg := usage.NewUsage(r, mu, rc)
-// 		err = usageclient.StartReporter(ctx, usageServiceClient, usagePB.Session_SERVICE_MODEL, config.Config.Server.Edition, version, usg.RetrieveUsageData)
-// 		if err != nil {
-// 			logger.Error(fmt.Sprintf("unable to start reporter: %v\n", err))
-// 		}
-// 	}()
-// }
+		usg := usage.NewUsage(r, mu, rc)
+		err = usageclient.StartReporter(ctx, usageServiceClient, usagePB.Session_SERVICE_MODEL, config.Config.Server.Edition, version, usg.RetrieveUsageData)
+		if err != nil {
+			logger.Error(fmt.Sprintf("unable to start reporter: %v\n", err))
+		}
+	}()
+}
 
 func main() {
 	if err := config.Init(); err != nil {
@@ -136,8 +141,8 @@ func main() {
 	triton := triton.NewTriton()
 	defer triton.Close()
 
-	// userServiceClient, userServiceClientConn := external.InitUserServiceClient()
-	// defer userServiceClientConn.Close()
+	userServiceClient, userServiceClientConn := external.InitUserServiceClient()
+	defer userServiceClientConn.Close()
 
 	pipelineServiceClient, pipelineServiceClientConn := external.InitPipelineServiceClient()
 	defer pipelineServiceClientConn.Close()
@@ -181,12 +186,12 @@ func main() {
 		panic(err)
 	}
 
-	// // Start usage reporter
-	// if !config.Config.Server.DisableUsage {
-	// 	usageServiceClient, usageServiceClientConn := external.InitUsageServiceClient()
-	// 	defer usageServiceClientConn.Close()
-	// 	startReporter(ctx, usageServiceClient, repository, userServiceClient, redisClient)
-	// }
+	// Start usage reporter
+	if !config.Config.Server.DisableUsage {
+		usageServiceClient, usageServiceClientConn := external.InitUsageServiceClient()
+		defer usageServiceClientConn.Close()
+		startReporter(ctx, usageServiceClient, repository, userServiceClient, redisClient)
+	}
 
 	var dialOpts []grpc.DialOption
 	if config.Config.Server.HTTPS.Cert != "" && config.Config.Server.HTTPS.Key != "" {
