8dedf5d2c77279a15f906df580a93c46b21cc046
phelan <phelan.nguyen@instill.tech>
2023-05-05 14:46:55 +0700

    fix: create single triton client (#357)
    Because
    
    - grpc triton client should not create per multipart request
    
    This commit
    
    - create a single triton client and pass to multipart handler methods

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -5,11 +5,14 @@ import (
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/instill-ai/model-backend/pkg/constant"
+	"github.com/instill-ai/model-backend/pkg/service"
 )
 
-func AppendCustomHeaderMiddleware(next runtime.HandlerFunc) runtime.HandlerFunc {
+type fn func(service.Service, http.ResponseWriter, *http.Request, map[string]string)
+
+func AppendCustomHeaderMiddleware(s service.Service, next fn) runtime.HandlerFunc {
 	return runtime.HandlerFunc(func(w http.ResponseWriter, r *http.Request, pathParams map[string]string) {
 		r.Header.Add(constant.HeaderOwnerIDKey, constant.DefaultOwnerID)
-		next(w, r, pathParams)
+		next(s, w, r, pathParams)
 	})
 }
