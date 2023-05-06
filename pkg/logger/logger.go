a6542ea9f129a4964f2c970feb592606544b1105
Phelan164 <phelan.nguyen@instill.tech>
2023-05-06 08:59:55 +0700

    chore: enable logging

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -3,8 +3,8 @@ package logger
 import (
 	"sync"
 
+	"github.com/instill-ai/model-backend/config"
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
 var logger *zap.Logger
@@ -13,16 +13,11 @@ var once sync.Once
 func GetZapLogger() (*zap.Logger, error) {
 	var err error
 	once.Do(func() {
-		// if config.Config.Server.Debug {
-		// 	logger, err = zap.NewDevelopment()
-		// } else {
-		// 	logger, err = zap.NewProduction()
-		// }
-		config := zap.NewProductionConfig()
-		config.DisableCaller = true
-		config.DisableStacktrace = true
-		config.Level = zap.NewAtomicLevelAt(zapcore.ErrorLevel)
-		logger, err = config.Build()
+		if config.Config.Server.Debug {
+			logger, err = zap.NewDevelopment()
+		} else {
+			logger, err = zap.NewProduction()
+		}
 	})
 
 	return logger, err
