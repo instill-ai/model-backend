172ddb103cf7cdf9c99ec2e16913ea51b032a5ea
Phelan164 <phelan.nguyen@instill.tech>
2023-05-11 14:21:55 +0700

    chore: update logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,23 +1,72 @@
 package logger
 
 import (
+	"os"
 	"sync"
 
 	"github.com/instill-ai/model-backend/config"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var logger *zap.Logger
 var once sync.Once
+var core zapcore.Core
 
+// GetZapLogger returns an instance of zap logger
 func GetZapLogger() (*zap.Logger, error) {
 	var err error
 	once.Do(func() {
+		// debug and info level enabler
+		debugInfoLevel := zap.LevelEnablerFunc(func(level zapcore.Level) bool {
+			return level == zapcore.DebugLevel || level == zapcore.InfoLevel
+		})
+
+		// info level enabler
+		infoLevel := zap.LevelEnablerFunc(func(level zapcore.Level) bool {
+			return level == zapcore.InfoLevel
+		})
+
+		// warn, error and fatal level enabler
+		warnErrorFatalLevel := zap.LevelEnablerFunc(func(level zapcore.Level) bool {
+			return level == zapcore.WarnLevel || level == zapcore.ErrorLevel || level == zapcore.FatalLevel
+		})
+
+		// write syncers
+		stdoutSyncer := zapcore.Lock(os.Stdout)
+		stderrSyncer := zapcore.Lock(os.Stderr)
+
+		// tee core
 		if config.Config.Server.Debug {
-			logger, err = zap.NewDevelopment()
+			core = zapcore.NewTee(
+				zapcore.NewCore(
+					zapcore.NewJSONEncoder(zap.NewDevelopmentEncoderConfig()),
+					stdoutSyncer,
+					debugInfoLevel,
+				),
+				zapcore.NewCore(
+					zapcore.NewJSONEncoder(zap.NewDevelopmentEncoderConfig()),
+					stderrSyncer,
+					warnErrorFatalLevel,
+				),
+			)
 		} else {
-			logger, err = zap.NewProduction()
+			core = zapcore.NewTee(
+				zapcore.NewCore(
+					zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
+					stdoutSyncer,
+					infoLevel,
+				),
+				zapcore.NewCore(
+					zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
+					stderrSyncer,
+					warnErrorFatalLevel,
+				),
+			)
 		}
+
+		// finally construct the logger with the tee core
+		logger = zap.New(core)
 	})
 
 	return logger, err
