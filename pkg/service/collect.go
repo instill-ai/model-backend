d40c56861a2002ba0af25121d4146904c67542ec
Heiru Wu <heiru.wu@instill.tech>
2023-08-02 03:23:30 +0800

    chore: fix if usage disabled

--- pkg/service/collect.go
+++ pkg/service/collect.go
@@ -5,17 +5,21 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/instill-ai/model-backend/config"
 	"github.com/instill-ai/model-backend/pkg/utils"
 )
 
 func (s *service) WriteNewDataPoint(ctx context.Context, data utils.UsageMetricData) error {
 
-	bData, err := json.Marshal(data)
-	if err != nil {
-		return err
-	}
+	if config.Config.Server.Usage.Enabled {
 
-	s.redisClient.RPush(ctx, fmt.Sprintf("user:%s:model.trigger_data", data.OwnerUID), string(bData))
+		bData, err := json.Marshal(data)
+		if err != nil {
+			return err
+		}
+
+		s.redisClient.RPush(ctx, fmt.Sprintf("user:%s:model.trigger_data", data.OwnerUID), string(bData))
+	}
 
 	return nil
 }
