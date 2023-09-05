f3f2e04a1fe3410f9009b442a3803c73cd33919b
Heiru Wu <heiru.wu@instill.tech>
2023-09-05 23:24:24 +0800

    chore: remove api-token validation

--- pkg/constant/constant.go
+++ pkg/constant/constant.go
@@ -3,5 +3,3 @@ package constant
 // Constants for resource owner
 const DefaultUserID string = "instill-ai"
 const HeaderUserUIDKey = "jwt-sub"
-const HeaderAuthorization = "Authorization"
-const AccessTokenKeyFormat = "access_token:%s:owner_permalink"
