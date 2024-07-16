f62cd40cfd535723b2814b239709ca9f7477f0c5
Jeremy Shih <jeremyshih0628@gmail.com>
2024-07-16 17:40:01 +0800

    refactor: added tests for service

--- pkg/mock/generator.go
+++ pkg/mock/generator.go
@@ -2,3 +2,6 @@ package mock
 
 //go:generate mockgen -destination service_mock.go -package $GOPACKAGE github.com/instill-ai/model-backend/pkg/service Service
 //go:generate mockgen -destination ray_mock.go -package $GOPACKAGE github.com/instill-ai/model-backend/pkg/ray Ray
+//go:generate mockgen -destination repository_mock.go -package $GOPACKAGE github.com/instill-ai/model-backend/pkg/repository Repository
+//go:generate mockgen -destination fga_client_mock.go -package $GOPACKAGE github.com/openfga/api/proto/openfga/v1 OpenFGAServiceClient
+//go:generate mockgen -destination mgmt_private_service_client_mock.go -package $GOPACKAGE github.com/instill-ai/protogen-go/core/mgmt/v1beta MgmtPrivateServiceClient
--- pkg/service/service_test.go
+++ pkg/service/service_test.go
@@ -1,19 +1,28 @@
 package service_test
 
-//go:generate mockgen -destination mock_repository_test.go -package $GOPACKAGE github.com/instill-ai/model-backend/pkg/repository Repository
-
 import (
 	"context"
+	"database/sql"
 	"testing"
 
+	"github.com/alicebob/miniredis/v2"
 	"github.com/gofrs/uuid"
 	"github.com/golang/mock/gomock"
+	"github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.einride.tech/aip/filtering"
+	"go.einride.tech/aip/ordering"
 
+	"github.com/instill-ai/model-backend/pkg/acl"
 	"github.com/instill-ai/model-backend/pkg/datamodel"
+	"github.com/instill-ai/model-backend/pkg/mock"
 	"github.com/instill-ai/model-backend/pkg/service"
 
+	commonpb "github.com/instill-ai/protogen-go/common/task/v1alpha"
+	mgmtpb "github.com/instill-ai/protogen-go/core/mgmt/v1beta"
 	modelPB "github.com/instill-ai/protogen-go/model/model/v1alpha"
+	openfga "github.com/openfga/api/proto/openfga/v1"
 )
 
 const ID = "modelID"
@@ -55,321 +64,122 @@ var ModelDefinition, _ = uuid.FromString("909c3278-f7d1-461c-9352-87741bef11d3")
 // 	})
 // }
 
-// func TestGetModelByID(t *testing.T) {
-// 	t.Run("TestGetModelByID", func(t *testing.T) {
-// 		ctrl := gomock.NewController(t)
+func TestGetModelByUID(t *testing.T) {
+	mr, err := miniredis.Run()
+	require.NoError(t, err)
+	defer mr.Close()
 
-// 		newModel := datamodel.Model{
-// 			BaseDynamic: datamodel.BaseDynamic{UID: uuid.UUID{}},
-// 			ID:          ID,
-// 			Description: sql.NullString{
-// 				String: "this is a test model",
-// 				Valid:  true,
-// 			},
-// 			ModelDefinitionUid: MODEL_DEFINITION,
-// 			Owner:              OWNER,
-// 		}
-// 		mockRepository := NewMockRepository(ctrl)
-// 		mockRepository.
-// 			EXPECT().
-// 			GetModelByID(gomock.Eq(OWNER), gomock.Eq(newModel.ID), modelPB.View_VIEW_FULL).
-// 			Return(datamodel.Model{}, nil).
-// 			Times(1)
-// 		s := service.NewService(mockRepository, nil, nil, nil, nil, nil)
+	rc := redis.NewClient(&redis.Options{
+		Addr: mr.Addr(),
+	})
 
-// 		_, err := s.GetModelByID(context.Background(), OWNER, newModel.ID, modelPB.View_VIEW_FULL)
-// 		assert.NoError(t, err)
-// 	})
-// }
+	t.Run("TestGetModelByUID", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
 
-// func TestGetModelByUID(t *testing.T) {
-// 	t.Run("TestGetModelByUID", func(t *testing.T) {
-// 		ctrl := gomock.NewController(t)
+		uid := uuid.UUID{}
+		newModel := datamodel.Model{
+			BaseDynamic: datamodel.BaseDynamic{UID: uid},
+			ID:          ID,
+			Description: sql.NullString{
+				String: "this is a test model",
+				Valid:  true,
+			},
+			ModelDefinitionUID: ModelDefinition,
+			Owner:              OWNER,
+			Task:               datamodel.ModelTask(commonpb.Task_TASK_CLASSIFICATION),
+		}
+		datamodel.TaskInputJSON = map[string]any{
+			commonpb.Task_TASK_CLASSIFICATION.String(): map[string]any{},
+		}
+		datamodel.TaskOutputJSON = map[string]any{
+			commonpb.Task_TASK_CLASSIFICATION.String(): map[string]any{},
+		}
 
-// 		uid := uuid.UUID{}
-// 		newModel := datamodel.Model{
-// 			BaseDynamic: datamodel.BaseDynamic{UID: uid},
-// 			ID:          ID,
-// 			Description: sql.NullString{
-// 				String: "this is a test model",
-// 				Valid:  true,
-// 			},
-// 			ModelDefinitionUid: MODEL_DEFINITION,
-// 			Owner:              OWNER,
-// 		}
-// 		mockRepository := NewMockRepository(ctrl)
-// 		mockRepository.
-// 			EXPECT().
-// 			GetModelByUID(gomock.Eq(OWNER), gomock.Eq(newModel.UID), modelPB.View_VIEW_FULL).
-// 			Return(datamodel.Model{}, nil).
-// 			Times(1)
-// 		s := service.NewService(mockRepository, nil, nil, nil, nil, nil)
+		ctx := context.Background()
+		mockRepository := mock.NewMockRepository(ctrl)
+		mockRepository.
+			EXPECT().
+			GetModelByUID(ctx, gomock.Eq(newModel.UID), false, false).
+			Return(&newModel, nil).
+			Times(1)
+		mockRepository.EXPECT().GetModelDefinitionByUID(newModel.ModelDefinitionUID).Return(&datamodel.ModelDefinition{}, nil).Times(1)
 
-// 		_, err := s.GetModelByUID(context.Background(), OWNER, uid, modelPB.View_VIEW_FULL)
-// 		assert.NoError(t, err)
-// 	})
-// }
+		wc := mock.NewMockOpenFGAServiceClient(ctrl)
+		wc.EXPECT().ListStores(gomock.Any(), gomock.Any()).Return(
+			&openfga.ListStoresResponse{
+				Stores: []*openfga.Store{
+					{Id: "id"},
+				},
+			},
+			nil).Times(1)
+		wc.EXPECT().ReadAuthorizationModels(gomock.Any(), gomock.Any()).Return(
+			&openfga.ReadAuthorizationModelsResponse{
+				AuthorizationModels: []*openfga.AuthorizationModel{
+					{Id: "id"},
+				},
+			},
+			nil).Times(1)
+		wc.EXPECT().Check(gomock.Any(), gomock.Any()).Return(&openfga.CheckResponse{Allowed: true}, nil).Times(3)
+		aclClient := acl.NewACLClient(wc, nil, rc)
 
-// func TestDeleteModel(t *testing.T) {
-// 	t.Run("TestDeleteModel", func(t *testing.T) {
-// 		ctrl := gomock.NewController(t)
+		mgmtPrivateService := mock.NewMockMgmtPrivateServiceClient(ctrl)
+		mgmtPrivateService.EXPECT().LookUpUserAdmin(gomock.Any(), gomock.Any(), gomock.Any()).Return(
+			&mgmtpb.LookUpUserAdminResponse{User: &mgmtpb.User{
+				Id: "id",
+			}},
+			nil).Times(2)
 
-// 		uid := uuid.UUID{}
-// 		newModel := datamodel.Model{
-// 			BaseDynamic:        datamodel.BaseDynamic{UID: uid},
-// 			ID:                 ID,
-// 			Description:        "this is a test model",
-// 			ModelDefinitionUid: MODEL_DEFINITION,
-// 			Owner:              OWNER,
-// 		}
-// 		mockRepository := NewMockRepository(ctrl)
-// 		mockRepository.
-// 			EXPECT().
-// 			GetModelByID(gomock.Eq(OWNER), gomock.Eq(newModel.ID), modelPB.View_VIEW_FULL).
-// 			Return(datamodel.Model{}, nil).
-// 			Times(1)
-// 		mockRepository.
-// 			EXPECT().
-// 			GetModelInstances(gomock.Eq(newModel.UID)).
-// 			Return([]datamodel.ModelInstance{}, nil).
-// 			Times(1)
-// 		mockRepository.
-// 			EXPECT().
-// 			DeleteModel(gomock.Eq(newModel.UID)).
-// 			Return(nil).
-// 			Times(1)
-// 		s := service.NewService(mockRepository, nil, nil, nil, nil)
+		s := service.NewService(mockRepository, nil, mgmtPrivateService, nil, rc, nil, nil, &aclClient, "")
 
-// 		err := s.DeleteModel(OWNER, ID)
-// 		assert.NoError(t, err)
-// 	})
-// }
+		_, err = s.GetModelByUID(ctx, uid, modelPB.View_VIEW_FULL)
+		assert.NoError(t, err)
+	})
+}
 
-// func TestRenameModel(t *testing.T) {
-// 	t.Run("TestRenameModel", func(t *testing.T) {
-// 		ctrl := gomock.NewController(t)
+func TestListModel(t *testing.T) {
+	mr, err := miniredis.Run()
+	require.NoError(t, err)
+	defer mr.Close()
 
-// 		uid := uuid.UUID{}
-// 		newModel := datamodel.Model{
-// 			BaseDynamic: datamodel.BaseDynamic{UID: uid},
-// 			ID:          ID,
-// 			Description: sql.NullString{
-// 				String: "this is a test model",
-// 				Valid:  true,
-// 			},
-// 			ModelDefinitionUid: MODEL_DEFINITION,
-// 			Owner:              OWNER,
-// 		}
+	rc := redis.NewClient(&redis.Options{
+		Addr: mr.Addr(),
+	})
 
-// 		mockRepository := NewMockRepository(ctrl)
-// 		mockRepository.
-// 			EXPECT().
-// 			GetModelByID(gomock.Eq(OWNER), gomock.Eq(newModel.ID), modelPB.View_VIEW_FULL).
-// 			Return(newModel, nil).
-// 			Times(1)
-// 		mockRepository.
-// 			EXPECT().
-// 			UpdateModel(newModel.UID, datamodel.Model{
-// 				ID: "new ID",
-// 			}).
-// 			Return(nil).
-// 			Times(1)
-// 		mockRepository.
-// 			EXPECT().
-// 			GetModelByID(gomock.Eq(OWNER), "new ID", modelPB.View_VIEW_FULL).
-// 			Return(datamodel.Model{
-// 				BaseDynamic: datamodel.BaseDynamic{UID: uid},
-// 				ID:          "new ID",
-// 				Description: sql.NullString{
-// 					String: "this is a test model",
-// 					Valid:  true,
-// 				},
-// 				ModelDefinitionUid: MODEL_DEFINITION,
-// 				Owner:              OWNER,
-// 			}, nil).
-// 			Times(1)
-// 		s := service.NewService(mockRepository, nil, nil, nil, nil, nil)
+	t.Run("TestListModel", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
 
-// 		_, err := s.RenameModel(context.Background(), OWNER, ID, "new ID")
-// 		assert.NoError(t, err)
-// 	})
-// }
+		mockRepository := mock.NewMockRepository(ctrl)
+		mockRepository.
+			EXPECT().
+			ListModels(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).
+			Return([]*datamodel.Model{}, int64(100), "", nil).
+			Times(1)
 
-// func TestPublishModel(t *testing.T) {
-// 	t.Run("TestPublishModel", func(t *testing.T) {
-// 		ctrl := gomock.NewController(t)
+		wc := mock.NewMockOpenFGAServiceClient(ctrl)
+		wc.EXPECT().ListStores(gomock.Any(), gomock.Any()).Return(
+			&openfga.ListStoresResponse{
+				Stores: []*openfga.Store{
+					{Id: "id"},
+				},
+			},
+			nil).Times(1)
+		wc.EXPECT().ReadAuthorizationModels(gomock.Any(), gomock.Any()).Return(
+			&openfga.ReadAuthorizationModelsResponse{
+				AuthorizationModels: []*openfga.AuthorizationModel{
+					{Id: "id"},
+				},
+			},
+			nil).Times(1)
+		wc.EXPECT().ListObjects(gomock.Any(), gomock.Any(), gomock.Any()).Return(&openfga.ListObjectsResponse{}, nil).Times(1)
+		aclClient := acl.NewACLClient(wc, nil, rc)
 
-// 		uid := uuid.UUID{}
-// 		newModel := datamodel.Model{
-// 			BaseDynamic: datamodel.BaseDynamic{UID: uid},
-// 			ID:          ID,
-// 			Description: sql.NullString{
-// 				String: "this is a test model",
-// 				Valid:  true,
-// 			},
-// 			ModelDefinitionUid: MODEL_DEFINITION,
-// 			Owner:              OWNER,
-// 		}
-// 		mockRepository := NewMockRepository(ctrl)
-// 		mockRepository.
-// 			EXPECT().
-// 			GetModelByID(gomock.Eq(OWNER), gomock.Eq(newModel.ID), modelPB.View_VIEW_FULL).
-// 			Return(newModel, nil).
-// 			Times(1)
-// 		mockRepository.
-// 			EXPECT().
-// 			GetModelByID(gomock.Eq(OWNER), gomock.Eq(newModel.ID), modelPB.View_VIEW_FULL).
-// 			Return(newModel, nil).
-// 			Times(1)
-// 		mockRepository.
-// 			EXPECT().
-// 			UpdateModel(newModel.UID, datamodel.Model{
-// 				ID:         ID,
-// 				Visibility: datamodel.ModelVisibility(modelPB.Model_VISIBILITY_PUBLIC),
-// 			}).
-// 			Return(nil).
-// 			Times(1)
-// 		s := service.NewService(mockRepository, nil, nil, nil, nil, nil)
+		s := service.NewService(mockRepository, nil, nil, nil, nil, nil, nil, &aclClient, "")
 
-// 		_, err := s.PublishModel(context.Background(), OWNER, ID)
-// 		assert.NoError(t, err)
-// 	})
-// }
-
-// func TestUnpublishModel(t *testing.T) {
-// 	t.Run("TestUnpublishModel", func(t *testing.T) {
-// 		ctrl := gomock.NewController(t)
-
-// 		uid := uuid.UUID{}
-// 		newModel := datamodel.Model{
-// 			BaseDynamic: datamodel.BaseDynamic{UID: uid},
-// 			ID:          ID,
-// 			Description: sql.NullString{
-// 				String: "this is a test model",
-// 				Valid:  true,
-// 			},
-// 			ModelDefinitionUid: MODEL_DEFINITION,
-// 			Owner:              OWNER,
-// 		}
-// 		mockRepository := NewMockRepository(ctrl)
-// 		mockRepository.
-// 			EXPECT().
-// 			GetModelByID(gomock.Eq(OWNER), gomock.Eq(newModel.ID), modelPB.View_VIEW_FULL).
-// 			Return(newModel, nil).
-// 			Times(1)
-// 		mockRepository.
-// 			EXPECT().
-// 			GetModelByID(gomock.Eq(OWNER), gomock.Eq(newModel.ID), modelPB.View_VIEW_FULL).
-// 			Return(newModel, nil).
-// 			Times(1)
-// 		mockRepository.
-// 			EXPECT().
-// 			UpdateModel(newModel.UID, datamodel.Model{
-// 				ID:         ID,
-// 				Visibility: datamodel.ModelVisibility(modelPB.Model_VISIBILITY_PRIVATE),
-// 			}).
-// 			Return(nil).
-// 			Times(1)
-// 		s := service.NewService(mockRepository, nil, nil, nil, nil, nil)
-
-// 		_, err := s.UnpublishModel(context.Background(), OWNER, ID)
-// 		assert.NoError(t, err)
-// 	})
-// }
-
-// func TestUpdateModel(t *testing.T) {
-// 	t.Run("TestUpdateModel", func(t *testing.T) {
-// 		ctrl := gomock.NewController(t)
-
-// 		uid := uuid.UUID{}
-// 		newModel := datamodel.Model{
-// 			BaseDynamic: datamodel.BaseDynamic{UID: uid},
-// 			ID:          ID,
-// 			Description: sql.NullString{
-// 				String: "this is a test model",
-// 				Valid:  true,
-// 			},
-// 			ModelDefinitionUid: MODEL_DEFINITION,
-// 			Owner:              OWNER,
-// 		}
-// 		mockRepository := NewMockRepository(ctrl)
-// 		mockRepository.
-// 			EXPECT().
-// 			UpdateModel(newModel.UID, newModel).
-// 			Return(nil).
-// 			Times(1)
-// 		mockRepository.
-// 			EXPECT().
-// 			GetModelByID(gomock.Eq(OWNER), gomock.Eq(newModel.ID), modelPB.View_VIEW_FULL).
-// 			Return(newModel, nil).
-// 			Times(1)
-// 		s := service.NewService(mockRepository, nil, nil, nil, nil, nil)
-
-// 		_, err := s.UpdateModel(context.Background(), newModel.UID, &newModel)
-// 		assert.NoError(t, err)
-// 	})
-// }
-
-// func TestListModell(t *testing.T) {
-// 	t.Run("TestListModel", func(t *testing.T) {
-// 		ctrl := gomock.NewController(t)
-
-// 		mockRepository := NewMockRepository(ctrl)
-// 		mockRepository.
-// 			EXPECT().
-// 			ListModels(OWNER, modelPB.View_VIEW_FULL, int(100), "").
-// 			Return([]datamodel.Model{}, "", int64(100), nil).
-// 			Times(1)
-// 		s := service.NewService(mockRepository, nil, nil, nil, nil, nil)
-
-// 		_, _, _, err := s.ListModels(context.Background(), OWNER, modelPB.View_VIEW_FULL, 100, "")
-// 		assert.NoError(t, err)
-// 	})
-// }
-
-// func TestModelInfer(t *testing.T) {
-// 	t.Run("ModelInfer", func(t *testing.T) {
-// 		ctrl := gomock.NewController(t)
-// 		mockRepository := NewMockRepository(ctrl)
-// 		triton := NewMockTriton(ctrl)
-// 		s := service.NewService(mockRepository, triton, nil, nil, nil, nil)
-
-// 		uid := uuid.UUID{}
-
-// 		ensembleModel := datamodel.TritonModel{
-// 			Name:    "ensembleModel",
-// 			Version: 1,
-// 		}
-// 		mockRepository.
-// 			EXPECT().
-// 			GetTritonEnsembleModel(uid).
-// 			Return(ensembleModel, nil)
-
-// 		modelConfigResponse := &inferenceserver.ModelConfigResponse{}
-// 		modelMetadataResponse := &inferenceserver.ModelMetadataResponse{}
-// 		modelInferResponse := &inferenceserver.ModelInferResponse{}
-// 		postResponse := []string{"1.0:dog:1"}
-// 		triton.
-// 			EXPECT().
-// 			ModelMetadataRequest(context.Background(), ensembleModel.Name, fmt.Sprint(ensembleModel.Version)).
-// 			Return(modelMetadataResponse)
-// 		triton.
-// 			EXPECT().
-// 			ModelConfigRequest(context.Background(), ensembleModel.Name, fmt.Sprint(ensembleModel.Version)).
-// 			Return(modelConfigResponse)
-// 		triton.
-// 			EXPECT().
-// 			ModelInferRequest(context.Background(), commonPB.Task_TASK_CLASSIFICATION, [][]byte{}, ensembleModel.Name, fmt.Sprint(ensembleModel.Version), modelMetadataResponse, modelConfigResponse).
-// 			Return(modelInferResponse, nil)
-// 		triton.
-// 			EXPECT().
-// 			PostProcess(modelInferResponse, modelMetadataResponse, commonPB.Task_TASK_CLASSIFICATION).
-// 			Return(postResponse, nil)
-
-// 		_, err := s.ModelInfer(context.Background(), uid, [][]byte{}, commonPB.Task_TASK_CLASSIFICATION)
-// 		assert.NoError(t, err)
-// 	})
-// }
+		_, _, _, err = s.ListModels(context.Background(), 100, "", modelPB.View_VIEW_FULL, nil,
+			filtering.Filter{}, false, ordering.OrderBy{})
+		assert.NoError(t, err)
+	})
+}
 
 // func TestDeployModelInstance(t *testing.T) {
 // 	t.Run("TestDeployModelInstance", func(t *testing.T) {
