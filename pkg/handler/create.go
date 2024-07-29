3205c60c384088bf4d3ba1c3ee0fa8b6dd3b59f3
Heiru Wu <heiru.wu@instill.tech>
2024-07-29 18:54:45 +0800

    refactor(pkg): allow private model creation

--- pkg/handler/create.go
+++ pkg/handler/create.go
@@ -2,14 +2,12 @@ package handler
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
 
 	"github.com/gofrs/uuid"
 	"go.opentelemetry.io/otel/trace"
-	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
@@ -18,7 +16,6 @@ import (
 	"github.com/instill-ai/model-backend/pkg/datamodel"
 	"github.com/instill-ai/model-backend/pkg/resource"
 	"github.com/instill-ai/model-backend/pkg/service"
-	"github.com/instill-ai/model-backend/pkg/utils"
 	"github.com/instill-ai/x/sterr"
 
 	custom_logger "github.com/instill-ai/model-backend/pkg/logger"
@@ -26,7 +23,7 @@ import (
 	modelpb "github.com/instill-ai/protogen-go/model/model/v1alpha"
 )
 
-func createContainerizedModel(s service.Service, ctx context.Context, model *modelpb.Model, ns resource.Namespace, modelDefinition *datamodel.ModelDefinition) (*modelpb.CreateNamespaceModelResponse, error) {
+func createContainerizedModel(s service.Service, ctx context.Context, ns resource.Namespace, modelDefinition *datamodel.ModelDefinition, model *modelpb.Model) (*modelpb.CreateNamespaceModelResponse, error) {
 
 	eventName := "CreateContainerizedModel"
 
@@ -38,76 +35,7 @@ func createContainerizedModel(s service.Service, ctx context.Context, model *mod
 
 	logger, _ := custom_logger.GetZapLogger(ctx)
 
-	var modelConfig datamodel.ContainerizedModelConfiguration
-	b, err := model.GetConfiguration().MarshalJSON()
-	if err != nil {
-		span.SetStatus(1, err.Error())
-		return &modelpb.CreateNamespaceModelResponse{}, status.Errorf(codes.InvalidArgument, err.Error())
-	}
-	if err := json.Unmarshal(b, &modelConfig); err != nil {
-		span.SetStatus(1, err.Error())
-		return &modelpb.CreateNamespaceModelResponse{}, status.Errorf(codes.InvalidArgument, err.Error())
-	}
-
-	bModelConfig, _ := json.Marshal(modelConfig)
-
-	containerizedModel, err := s.PBToDBModel(ctx, ns, model)
-	if err != nil {
-		return nil, err
-	}
-	containerizedModel.Configuration = bModelConfig
-	containerizedModel.ModelDefinitionUID = modelDefinition.UID
-
-	// modelMeta := utils.ModelMeta{
-	// 	Tags: []string{"Containerized", "Experimental"},
-	// 	Task: model.Task.String(),
-	// }
-
-	containerizedModel.Task = datamodel.ModelTask(utils.Tasks[model.Task.String()])
-	// if val, ok := utils.Tasks[fmt.Sprintf("TASK_%v", strings.ToUpper(modelMeta.Task))]; ok {
-	// 	containerizedModel.Task = datamodel.ModelTask(val)
-	// } else {
-	// 	if modelMeta.Task != "" {
-	// 		st, err := sterr.CreateErrorResourceInfo(
-	// 			codes.FailedPrecondition,
-	// 			"[handler] create a model error: unsupported task",
-	// 			"request body",
-	// 			"request body contains unsupported task",
-	// 			"",
-	// 			"",
-	// 		)
-	// 		if err != nil {
-	// 			logger.Error(err.Error())
-	// 		}
-	// 		span.SetStatus(1, st.Err().Error())
-	// 		return &modelpb.Model{}, st.Err()
-	// 	} else {
-	// 		containerizedModel.Task = datamodel.ModelTask(commonpb.Task_TASK_UNSPECIFIED)
-	// 	}
-	// }
-
-	// TODO: properly support batch inference
-	maxBatchSize := 0
-	allowedMaxBatchSize := utils.GetSupportedBatchSize(containerizedModel.Task)
-
-	if maxBatchSize > allowedMaxBatchSize {
-		st, e := sterr.CreateErrorPreconditionFailure(
-			"[handler] create a model",
-			[]*errdetails.PreconditionFailure_Violation{
-				{
-					Type:        "MAX BATCH SIZE LIMITATION",
-					Subject:     "Create a model error",
-					Description: fmt.Sprintf("The max_batch_size in config.pbtxt exceeded the limitation %v, please try with a smaller max_batch_size", allowedMaxBatchSize),
-				},
-			})
-		if e != nil {
-			logger.Error(e.Error())
-		}
-		span.SetStatus(1, st.Err().Error())
-		return &modelpb.CreateNamespaceModelResponse{}, st.Err()
-	}
-
-	if err := s.CreateNamespaceModel(ctx, ns, containerizedModel); err != nil {
+	if err := s.CreateNamespaceModel(ctx, ns, modelDefinition, model); err != nil {
 		st, err := sterr.CreateErrorResourceInfo(
 			codes.Internal,
 			fmt.Sprintf("[handler] create a model error: %s", err.Error()),
@@ -136,7 +64,6 @@ func createContainerizedModel(s service.Service, ctx context.Context, model *mod
 		span,
 		logUUID.String(),
 		eventName,
-		custom_otel.SetEventResource(containerizedModel),
 	)))
 
 	return &modelpb.CreateNamespaceModelResponse{
